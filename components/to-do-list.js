f411af6e6005f170f353bfe736ec43e2c338be53
Denuke <ddnadhananja@gmail.com>
2022-06-12 01:54:22 +0530

    Added DUMMY data-set and added filterd Tasks

--- components/to-do-list.js
+++ components/to-do-list.js
@@ -2,25 +2,52 @@ import styles from '../styles/to-do-list.module.css';
 import TodoItem from '../components/to-do-item';
 
 const TodoList = () => {
+    const taskList = [
+        {
+          id: "1",
+          task: "Complete the task before tuesday",
+          status: "TODO",
+        },
+        {
+          id: "2",
+          task: "Go shopping",
+          status: "INGOING",
+        },
+        {
+          id: "3",
+          task: "Buy Milk",
+          status: "DONE",
+        },
+        {
+          id: "4",
+          task: "Study for the end Exam",
+          status: "TODO",
+        },
+    ]
+    
+    const TODOLIST = taskList.filter(task => task.status === "TODO");
+    const INGOINGLIST = taskList.filter(task => task.status === "INGOING");
+    const DONELIST = taskList.filter(task => task.status === "DONE");
+
     return (
         <div className={ styles.container }>
             <div className={`${styles.section} ${styles.todo}` }>
                 <p className={styles.sectionTitle}>Todo Tasks</p>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem/>
+                {TODOLIST.map(item => (
+                    <TodoItem key={item.id} item={item} />
+                ))}
             </div>
             <div className={styles.section}>
                 <p className={styles.sectionTitle}>Ongoing Tasks</p>
-                <TodoItem />
-                <TodoItem/>
+                {INGOINGLIST.map(item => (
+                    <TodoItem key={item.id}  item={item} />
+                ))}
             </div>
             <div className={styles.section}>
                 <p className={styles.sectionTitle}>Completed Tasks</p>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-                <TodoItem/>
+                {DONELIST.map(item => (
+                    <TodoItem key={item.id}  item={item} />
+                ))}
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>Todo | App</title>
       </Head>
-      <TodoList/>
+      <TodoList />
     </>
   )
 }
