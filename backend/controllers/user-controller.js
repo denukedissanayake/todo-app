cc59169714407dde9d2b6a3565d41a3efc10d30c
Denuke <ddnadhananja@gmail.com>
2022-06-14 03:53:42 +0530

    Added password encryption

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,32 +1,38 @@
 const models = require('../models');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const login = async (req, res) => {
 
-    models.User.findOne({ where: { username: req.body.username } }).then(result => {
+    try {
+        const result = await models.User.findOne({ where: { username: req.body.username } })
         if (!result) {
             return res.json("NOT-FOUND").status(404)
         } else {
-            if (result.password === req.body.password) {
+            const isPasswordCorrect = await bcrypt.compare(req.body.password, result.password);
+
+            if (isPasswordCorrect) {
                 const accesToken = jwt.sign({
                     username: req.body.username
                 }, process.env.JWT_SECRET, {
-                    expiresIn : "1d"
+                    expiresIn: "1d"
                 })
                 return res.json(accesToken).status(200)
             } else {
                 return res.json("INVALID-PASSWORD").status(500)
             }
         }
-    }).catch(err => {
+    } catch(err) {
         return res.json("ERROR").status(500)
-    })
+    }
 }
 
 const signup = async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const user = {
         username: req.body.username,
-        password: req.body.password,
+        password: hashedPassword,
     }
 
     models.User.create(user).then(result => {
